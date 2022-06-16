6e1eb0cbc43d0455654118ca4dd25180ac1a2f8a
GabrielM1999 <90460021+GabrielM1999@users.noreply.github.com>
2022-06-15 23:49:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gew
+Grewayne
+# Foobar
+
+Foobar is a Python library for dealing with word pluralization.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install foobar
+```
+
+## Usage
+
+```javascript
+import foobar
+
+Gabriel Melo dos Santos 
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ dados.js
@@ -0,0 +1,23 @@
+//alert("Cuidado!!! Essa pagina pode acessar seus dados");
+var btn = document.getElementById("zeca"); // Tem como função procurar/buscar O elemento no HTML de origem
+
+ /*
+ ################### Esses botoes são direcionados para o HTML funcionam como função de back #############*/
+
+function botum(){ // FUNCTION + NOME + (); tem como função declarar uma nova função/aplicação 
+    document.write ("Suporte"); //document.write (); tem como função de acrescimo de texto  
+}
+function botdois(){ //Tiago
+    document.write ("Dev C");
+}
+function bottres(){ //Vinicius
+    document.write ("Back-End");
+}
+function botquatro(){ //Ricardo
+    document.write ("Front-End");
+}
+function botcinco(){ //Felipe
+    document.write ("Java");
+}
+
+
